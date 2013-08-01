7a57f435b36cd6760930b76c47acd6d0285ab3f9
Arthur Maia Vargas <Vargas@Arthur-Maia-Vargass-MacBook-Pro.local>
2013-07-31 23:19:46 -0300

    Add Admin directory + path adjust

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Paporeto::Application.routes.draw do
   devise_for :users
-  resources :categories
-  resources :users
-  resources :articles, path: 'artigos' , path_names: {new: 'novo', edit: 'editar'}
-  root 'articles#index'
+  
+  namespace :admin do
+    resources :categories
+    resources :users
+    resources :articles, path: 'artigos' , path_names: {new: 'novo', edit: 'editar'}
+  end
+  
+  root 'admin/articles#index'
+  
   mount Uploadbox::Engine => '/uploadbox', as: :uploadbox
 end
